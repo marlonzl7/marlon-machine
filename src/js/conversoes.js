638f57b99242e4c898e2106a6a5098b71cbd91fe
marlonzl7 <marlon.silva@sptech.school>
2025-09-22 19:08:43 -0300

    feat(js/conversoes): adicionar função de conversão manual de decimal para binário

--- src/js/conversoes.js
+++ src/js/conversoes.js
@@ -1,7 +1,7 @@
 function converterBinarioParaDecimal(binario) {
     let decimal = 0;
 
-    for (i = binario.length - 1; i >= 0; i--) {
+    for (let i = binario.length - 1; i >= 0; i--) {
         const j = binario.length - i - 1;
         decimal += (binario[i] === 1) ? Math.pow(2, j) : 0;
     }
@@ -10,6 +10,25 @@ function converterBinarioParaDecimal(binario) {
 } 
 
 function converterDecimalParaBinario(decimal) {
+    let k =  Math.floor(Math.log(decimal) / Math.log(2)) + 1;
+    let binario = new Array(k);
 
+    if (decimal <= 0) {
+        return binario;
+    }
+
+    resto = decimal;
+
+    for (let i = binario.length; i >= 0; i--) {
+        let potencia = Math.pow(2, i);
+
+        binario[binario.length - i - 1] = (resto - potencia >= 0) ? 1 : 0;
+
+        if (binario[binario.length - i - 1] == 1) {
+            resto -= potencia;
+        }
+    }
+
+    return binario;
 }
 
